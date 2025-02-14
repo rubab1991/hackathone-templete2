06b98d7ab33f2acd7a8aad9eed1ac03727d86e26
Rubab Bukhari <161203406+rubab1991@users.noreply.github.com>
2025-02-15 00:54:50 +0500

    20

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,58 +1,45 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+
 import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
 import { Product } from "../../../../types/products";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
-import { useParams } from "next/navigation"; // Use useParams from next/navigation
+
 
 interface ProductPageProps {
-  product?: Product | null;
+params : Promise<{slug : string}>
 }
 
-const getProduct = async (slug: string): Promise<Product | null> => {
+async function getProduct  (slug: string): Promise <Product> {
   return client.fetch(
     groq`*[_type == "product" && slug.current == $slug][0]{
       _id,
       name,
+      _type,
       image,
       price,
       description
+    
     }`,
     { slug }
   );
 };
 
-const ProductPage = ({ product }: ProductPageProps) => {
-  const { slug } = useParams(); // Get slug from URL params
-
-  const [productData, setProductData] = useState<Product | null>(product ?? null);
-
-  useEffect(() => {
-    if (slug && !productData) {
-      const fetchProduct = async () => {
-        const data = await getProduct(slug as string);
-        setProductData(data);
-      };
-      fetchProduct();
-    }
-  }, [slug, productData]);
-
-  if (!productData) {
-    return <div>Loading...</div>;
-  }
-
+export default async function ProductPage ({ params }: ProductPageProps)  {
+  const { slug } = await params;
+const product = await getProduct(slug)
+ 
   return (
     <div className="max-w-7xl mx-auto px-4">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
         {/* Product Image */}
         <div className="aspect-square">
-          {productData.image && (
+          {product.image && (
             <Image
-              src={urlFor(productData.image).url()}
-              alt={productData.name}
+              src={urlFor(product.image).url()}
+              alt={product.name}
               width={400}
               height={400}
               className="rounded-lg shadow-md"
@@ -62,13 +49,13 @@ const ProductPage = ({ product }: ProductPageProps) => {
 
         {/* Product Details */}
         <div className="flex flex-col gap-8">
-          <h1 className="text-4xl font-bold">{productData.name}</h1>
-          <p className="text-2xl font-sans">${productData.price}</p>
-          <p className="text-lg font-sans">{productData.description}</p>
+          <h1 className="text-4xl font-bold">{product.name}</h1>
+          <p className="text-2xl font-sans">${product.price}</p>
+          <p className="text-lg font-sans">{product.description}</p>
         </div>
       </div>
     </div>
   );
 };
 
-export default ProductPage;
+
